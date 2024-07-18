5bb1c9c4f90fc024dfc6994ef04a1da24eaec6eb
AMorales2308 <andersondavid000.admc@gmail.com>
2024-07-17 22:08:43 -0500

    Implementation of model database of Commerce

--- auctions/models.py
+++ auctions/models.py
@@ -9,6 +9,21 @@ class Listing(models.Model):
     title = models.CharField(max_length=25)
     description = models.TextField()
     starting_bid = models.DecimalField(max_digits=10, decimal_places=4)
+    is_closed = models.BooleanField(default=False)
     url_image = models.CharField(max_length=100, null=True)
     category = models.CharField(max_length=30, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
+
+class Bid(models.Model):
+    value = models.DecimalField(max_digits=10, decimal_places=4)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
